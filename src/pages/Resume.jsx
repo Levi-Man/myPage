6454c5e23bb6a06c57fb0f7771623168c5b317b5
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2024-02-01 21:30:53 -0500

    Fixed error where pdf was not displaying

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -6,9 +6,9 @@ export default function Resume() {
       <h2>Resume</h2>
       <div className="d-flex">
         {/* Adjust the width attribute to control the width of the embedded PDF */}
-        <embed src="../../public/Indra Levi Manahan.pdf" type="application/pdf" width="1024px" height="600px" />
+        <embed src="/Indra Levi Manahan.pdf" type="application/pdf" width="1024px" height="600px" />
       </div>
-      <a href={'../../public/Indra Levi Manahan.pdf'} download>
+      <a href={'/Indra Levi Manahan.pdf'} download>
         Download Resume
       </a>
     </div>
