2453fbfd00ad16762e5058e03a1f0da687cdeee1
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2024-02-01 23:20:30 -0500

    Fixed spacing on Resume section

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -6,7 +6,7 @@ export default function Resume() {
       <h2>Resume</h2>
       <div className="d-flex">
         {/* Adjust the width attribute to control the width of the embedded PDF */}
-        <embed src="/Indra Levi Manahan.pdf" type="application/pdf" width="1024px" height="600px" />
+        <embed src="/Indra Levi Manahan.pdf" type="application/pdf" width="90%" height="600px" />
       </div>
       <a href={'/Indra Levi Manahan.pdf'} download>
         Download Resume
