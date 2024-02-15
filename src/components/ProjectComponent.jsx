35f0986e4f4e86769137639ff8b60f78a26df8a0
Indra Levi Manahan <indra.levi.manahan@gmail.com>
2024-02-15 15:43:03 -0500

    Updated resume and added to folio

--- src/components/ProjectComponent.jsx
+++ src/components/ProjectComponent.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './style/ProjectComponent.css';
 
-const ProjectComponent = ({ title, imageSrc, link }) => {
+const ProjectComponent = ({ title, imageSrc, link, linkGit }) => {
   return (
     <div className="project-card">
       <h3>{title}</h3>
       <img src={imageSrc} alt={`${title} Screenshot`} className="project-image" />
       <a href={link} target="_blank" rel="noopener noreferrer">
-        View Project
+        Deployed Project 
+      </a><br/>
+      <a href={linkGit} target="_blank" rel="noopener noreferrer">
+        GitHub Repo 
       </a>
     </div>
   );
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -8,6 +8,7 @@ import project3Image from '/passwordGenerator.png';
 import project4Image from '/employeeManager.png';
 import project5Image from '/camelDailyOrganizer.png';
 import project6Image from '/scheduler.png';
+import project7Image from '/bikeMe.png';
 
 const Portfolio = () => {
   return (
@@ -17,31 +18,36 @@ const Portfolio = () => {
         <ProjectComponent
           title="Wonderwall"
           imageSrc={project1Image}
-          link="https://salty-lake-79772-1b4cdf7bf6b9.herokuapp.com/"
+          link="https://salty-lake-79772-1b4cdf7bf6b9.herokuapp.com/" linkGit="https://github.com/Levi-Man/Wonderwall"
+        />
+        <ProjectComponent
+          title="BikeME"
+          imageSrc={project7Image}
+          link="https://bikeme.onrender.com/" linkGit="https://github.com/Levi-Man/bikeME"
         />
         <ProjectComponent
           title="JATE"
           imageSrc={project2Image}
-          link="https://ilm-jate.onrender.com/"
+          link="https://ilm-jate.onrender.com/" linkGit="https://github.com/Levi-Man/JATE"
         />
         <ProjectComponent
           title="Password Generator"
-          imageSrc={project3Image}
+          imageSrc={project3Image} linkGit="https://github.com/Levi-Man/passwordGenerator"
           link="https://levi-man.github.io/passwordGenerator/"
         />
         <ProjectComponent
           title="Employee Manager"
-          imageSrc={project4Image}
+          imageSrc={project4Image} linkGit="https://github.com/Levi-Man/employee_manager"
           link="https://github.com/Levi-Man/employee_manager"
         />
         <ProjectComponent
           title="Camel Daily Organizer"
-          imageSrc={project5Image}
+          imageSrc={project5Image} linkGit="https://github.com/Levi-Man/camel_daily_organizer"
           link="https://levi-man.github.io/camel_daily_organizer/"
         />
         <ProjectComponent
           title="Scheduler"
-          imageSrc={project6Image}
+          imageSrc={project6Image} linkGit="https://github.com/Levi-Man/Scheduler"
           link="https://levi-man.github.io/Scheduler/"
         />
       </div>
